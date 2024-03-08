d0f2c89196e90f74c576176e670a0eb17b8fe9d3
Elena Bashurova <110354016+ElenaBashurova@users.noreply.github.com>
2024-03-08 22:47:07 +0300

    Последние изменения

--- diploma_project/pages/product_select_page.py
+++ diploma_project/pages/product_select_page.py
@@ -2,7 +2,6 @@ import allure
 from selene import browser, have
 
 
-
 class PageProduct:
     def open_page(self):
         with allure.step('Открыть страницу https://www.officemag.ru'):
@@ -12,7 +11,6 @@ class PageProduct:
             browser.element('#fancybox-close').click()
         return self
 
-
     def search_product_enter(self, product_name):
         with allure.step('На странице сделать клик на кнопку поиска'):
             browser.element('[placeholder="Поиск по названию или коду товара"]').click()
@@ -21,7 +19,8 @@ class PageProduct:
         with allure.step('Выбрать товар'):
             browser.element("#bx_2973280320_8349739").click()
         with allure.step('Товар найден на странице'):
-            browser.element('.ProductHead__name').should(have.text('Кресло BRABIX «Tender MG-330», с подлокотниками, хром, черное, 531845'))
+            browser.element('.ProductHead__name').should(
+                have.text('Кресло BRABIX «Tender MG-330», с подлокотниками, хром, черное, 531845'))
         return self
 
     def search_part_product(self, part_name):
@@ -32,7 +31,8 @@ class PageProduct:
         with allure.step('Выбрать товар'):
             browser.element("#bx_2973280320_262114").click()
         with allure.step('Товар найден на странице'):
-            browser.element('.ProductHead__name').should(have.text('Шкаф (стеллаж) «Канц», 700×330×1830 мм, 4 полки, цвет бук невский, ШК31.10'))
+            browser.element('.ProductHead__name').should(
+                have.text('Шкаф (стеллаж) «Канц», 700×330×1830 мм, 4 полки, цвет бук невский, ШК31.10'))
         return self
 
     def search_foreign_product(self, foreign_name):
